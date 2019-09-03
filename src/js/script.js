498b7bf26cfc6f95b721ab6907ef21a2c4449ff9
Maurici <hello@mauriciabad.com>
2019-09-03 10:00:53 +0200

    Basic js

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const inputElement = document.getElementById('input-prefix-proposition');
+const outputElement = document.getElementById('output');
+
+var input = inputElement.value;
+var output = 'test';
+
+outputElement.textContent = output;
