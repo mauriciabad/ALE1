2c2d47a2f9d90a18c69cdb059c2cdee078a31b7f
Maurici <hello@mauriciabad.com>
2019-09-03 14:34:44 +0200

    v1

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,25 @@ const inputElement = document.getElementById('input-prefix-proposition');
 const outputElement = document.getElementById('output');
 
 var input = inputElement.value;
-var output = 'test';
+var output = '';
+
+const values = {
+  A: true,
+  B: false,
+  C: false,
+  D: false,
+  E: false,
+  F: false,
+  G: false,
+  H: false,
+}
+
+var proposition = Gate.parse(input);
+var variables = proposition.variables.reduce(function(result, variableName, i) {
+  result[i] = variableName;
+  return result;
+}, {})
+console.log(variables);
+output = proposition.evaluate();
 
 outputElement.textContent = output;
---
+++ src/ts/gate.ts
@@ -0,0 +1,78 @@
+type operator = unaryOperator | binaryOperator
+type unaryOperator = '¬'
+type binaryOperator = '|' | '&' | '=' | '>'
+type objectOfBooleans = {[s: string]: boolean}
+
+class Gate {
+  operator:operator
+  operands:(Gate|Value)[]
+
+  constructor(operator:operator, operands:(Gate|Value)[]) {
+    this.operator = operator
+    this.operands = operands
+  }
+
+  evaluate(values:objectOfBooleans):boolean {
+    switch (this.operator) {
+      case '¬': return !this.operands[0].evaluate(values)    
+      case '|': return this.operands[0].evaluate(values) || this.operands[1].evaluate(values)
+      case '&': return this.operands[0].evaluate(values) && this.operands[1].evaluate(values)
+      case '=': return this.operands[0].evaluate(values) && this.operands[1].evaluate(values) // TODO: this is a stub, use the actual function
+      case '>': return this.operands[0].evaluate(values) && this.operands[1].evaluate(values) // TODO: this is a stub, use the actual function
+    }
+  }
+
+  static parse(rawData:string): Gate|Value{
+    rawData = rawData.replace(/\s/g, '');
+    let operator:operator = rawData.match(/$[¬|&=>]/)[0] as operator
+    if(operator){
+      let operands:string[] = []
+      let level = 0;
+      let operandBegining = 2
+      for (let i = 2; i < rawData.length - 1; i++) {
+        switch (rawData[i]) {
+          case '(': ++level; break;
+          case ')': --level; break;
+          case ',': 
+            if(level === 0) {
+              operands.push(rawData.substr(operandBegining,i))
+              operandBegining = i + 1;
+            }
+            break;
+        }
+      }
+      if(level !== 0) throw "Error parsing Gate, parenthesis are wrong.";
+      return new Gate(operator, operands.map((operand) => Gate.parse(operand)))
+    }else{
+      return new Value(rawData)
+    }
+  }
+  
+  get variables():string[]{
+    let variables = []
+    this.getVariablesRecursive(variables)
+    return variables
+  }
+  
+  getVariablesRecursive(variables:string[]) {
+    for (const operand of this.operands) {
+      operand.getVariablesRecursive(variables)
+    }
+  }
+}
+
+class Value{
+  name:string
+
+  constructor(name:string) {
+    this.name = name
+  }
+
+  evaluate(values:objectOfBooleans) {
+    return values[this.name]
+  }
+  getVariablesRecursive(variables:string[]) {
+    if(!variables.includes(this.name)) variables.push(this.name)
+  }
+
+}
